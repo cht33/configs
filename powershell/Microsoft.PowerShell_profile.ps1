8c6f93363fa65f19f069da23e6f9ba4dab774f00
cht33 <chenhaitian233@gmail.com>
2021-09-12 15:03:54 +0800

    posh: 修改历史搜索命令的快捷键，保持和zsh一致。

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -21,19 +21,19 @@ Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
 # 设置 Ctrl+z 为撤销
 Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
 
-# 设置向上键/<C-k>为后向搜索历史记录
+# 设置向上键/<C-p>为后向搜索历史记录
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Key "Ctrl+k" -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function HistorySearchBackward
 
-# 设置向下键/<C-j>为前向搜索历史纪录
+# 设置向下键/<C-n>为前向搜索历史纪录
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Set-PSReadLineKeyHandler -Key "Ctrl+j" -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function HistorySearchForward
 
 # 设置<C-f>为接受当前补全提示
 Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function AcceptSuggestion
 
 # 设置<C-l>为接受下一个补全提示词
-Set-PSReadLineKeyHandler -Key "Ctrl+l" AcceptNextSuggestionWord
+Set-PSReadLineKeyHandler -Key "Alt+f" AcceptNextSuggestionWord
 
 # 重定义Rightarrow，处于行尾时为接受下一个补全提示词
 Set-PSReadLineKeyHandler -Key RightArrow -ScriptBlock {
@@ -67,30 +67,32 @@ function OpenCurrentFolder {
 Set-Alias -Name open -Value OpenCurrentFolder
 
 # git alias
-function Get-GitStatus { & git status -sb $args } 
-New-Alias -Name gs -Value Get-GitStatus -Force -Option AllScope 
-function Get-GitCommit { & git commit } 
-New-Alias -Name gco -Value Get-GitCommit -Force -Option AllScope 
-function Get-GitCommitAmend { & git commit --amend $args } 
-New-Alias -Name gca -Value Get-GitCommitAmend -Force -Option AllScope 
-function Get-GitAddAll { & git add --all $args } 
-New-Alias -Name gaa -Value Get-GitAddAll -Force -Option AllScope 
-function Get-GitAdd { & git add -- $args } 
-New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope 
-function Get-GitTree { & git log --graph --oneline --decorate $args } 
-New-Alias -Name gt -Value Get-GitTree -Force -Option AllScope 
-function Get-GitPush { & git push --follow-tags $args } 
-New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope 
-function Get-GitPullRebase { & git pull $args } 
-New-Alias -Name gpr -Value Get-GitPullRebase -Force -Option AllScope 
-function Get-GitFetch { & git fetch $args } 
-New-Alias -Name gf -Value Get-GitFetch -Force -Option AllScope 
-function Get-GitCheckout { & git checkout $args } 
-New-Alias -Name gch -Value Get-GitCheckout -Force -Option AllScope 
-function Get-GitCheckoutBranch { & git checkout -b $args } 
-New-Alias -Name gchb -Value Get-GitCheckoutBranch -Force -Option AllScope 
-function Get-GitBranch { & git branch $args } 
-New-Alias -Name gb -Value Get-GitBranch -Force -Option AllScope 
+function Get-GitStatus { & git status -sb $args }
+New-Alias -Name gs -Value Get-GitStatus -Force -Option AllScope
+function Get-GitDiff { & git diff $args }
+New-Alias -Name gd -Value Get-GitDiff -Force -Option AllScope
+function Get-GitCommit { & git commit }
+New-Alias -Name gco -Value Get-GitCommit -Force -Option AllScope
+function Get-GitCommitAmend { & git commit --amend $args }
+New-Alias -Name gca -Value Get-GitCommitAmend -Force -Option AllScope
+function Get-GitAddAll { & git add --all $args }
+New-Alias -Name gaa -Value Get-GitAddAll -Force -Option AllScope
+function Get-GitAdd { & git add -- $args }
+New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
+function Get-GitTree { & git log --graph --oneline --decorate $args }
+New-Alias -Name gt -Value Get-GitTree -Force -Option AllScope
+function Get-GitPush { & git push --follow-tags $args }
+New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope
+function Get-GitPullRebase { & git pull $args }
+New-Alias -Name gpr -Value Get-GitPullRebase -Force -Option AllScope
+function Get-GitFetch { & git fetch $args }
+New-Alias -Name gf -Value Get-GitFetch -Force -Option AllScope
+function Get-GitCheckout { & git checkout $args }
+New-Alias -Name gch -Value Get-GitCheckout -Force -Option AllScope
+function Get-GitCheckoutBranch { & git checkout -b $args }
+New-Alias -Name gchb -Value Get-GitCheckoutBranch -Force -Option AllScope
+function Get-GitBranch { & git branch $args }
+New-Alias -Name gb -Value Get-GitBranch -Force -Option AllScope
 #-------------------------------    Set Alias END     ------------------------------
 
 function prompt {
